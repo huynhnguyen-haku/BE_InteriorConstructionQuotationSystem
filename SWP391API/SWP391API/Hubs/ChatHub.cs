40eb86d60e99886ce947cfbf52fc7262e81dd64e
hnamredips <nghnam265@gmail.com>
2024-04-04 17:42:49 +0700

    add signalR chathub, notification hub

---
+++ SWP391API/SWP391API/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using SWP391API.Services;
+using SWP391API.Services.Implements;
+
+namespace SWP391API.Hubs
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class ChatHub : Hub
+    {
+
+        private readonly IAuthenticateService _authenticateService;
+
+        public ChatHub(IAuthenticateService authenticateService)
+        {
+            _authenticateService = authenticateService;
+        }
+
+        public async Task JoinChatRoom(string roomId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+        }
+
+        public async Task SendMessage(string roomId, string message)
+        {
+            var username = _authenticateService.getCurrentUsername();
+
+            await Clients.Group(roomId).SendAsync("ReceiveMessage", username!, message);
+        }
+    }
+}
---
+++ SWP391API/SWP391API/Hubs/NotificationHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SWP391API.Hubs
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class NotificationHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendToUser(string user, string message)
+        {
+            await Clients.User(user).SendAsync("NotifyMessage", user, message);
+        }
+
+    }
+}
