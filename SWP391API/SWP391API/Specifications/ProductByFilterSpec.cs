f7f39ff122e8c8224ee48963a92c6c664d4889b8
hnamredips <nghnam265@gmail.com>
2024-04-04 17:42:02 +0700

    add product specification

---
+++ SWP391API/SWP391API/Specifications/ProductByFilterSpec.cs
@@ -0,0 +1,21 @@
+﻿using Ardalis.Specification;
+using SWP391API.DTO;
+using SWP391API.Models;
+
+namespace SWP391API.Specifications
+{
+    public class ProductByFilterSpec : Specification<Product>
+    {
+        public ProductByFilterSpec(ProductFilterDTO productFilterDTO)
+        {
+            Query
+                .Include(p => p.Category)
+                .Include(p => p.User)
+                .Search(p => p.Name, "%" + productFilterDTO.SearchName + "%", productFilterDTO.SearchName != null)
+                .Where(p => p.Price >= productFilterDTO.MinPrice, productFilterDTO.MinPrice != null)
+                .Where(p => p.Price <= productFilterDTO.MaxPrice, productFilterDTO.MaxPrice != null)
+                .Where(p => p.CategoryId == productFilterDTO.CategoryId, productFilterDTO.CategoryId != null)
+                .OrderByDescending(p => p.CreatedAt, productFilterDTO.SortByDateDescending);
+        }
+    }
+}
---
+++ SWP391API/SWP391API/Specifications/ProductByIdSpec.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.Specification;
+using SWP391API.Models;
+
+namespace SWP391API.Specifications
+{
+    public class ProductByIdSpec : Specification<Product>
+    {
+        public ProductByIdSpec(int id)
+        {
+            Query
+                .Include(p => p.Category)
+                .Include(p => p.User)
+                .Include(p => p.QuotationTemps)
+                .Include(p => p.QuotationDetails)
+                .Include(p => p.ProductInProjects)
+                .Where(p => p.ProductId == id);
+        }
+    }
+}
